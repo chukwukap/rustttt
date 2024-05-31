70fc5834dffee281dac91897217dc01ecbba6b3c
Uba chukwuka promise <promiseuba67@gmail.com>
2024-05-31 04:20:27 +0100

    rustttt

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+/**
+ * Go deeper into:
+ * Managing Memory
+ *
+ *
+ * When you create a variable in a function, that function owns the variable unless you borrow or move it from the function.
+ */
+
+fn main() {}
